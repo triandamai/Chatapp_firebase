7afb9ac5a996eac6fd26be0ceb8a6deec62d2f57
trian <triannurizkillah@gmail.com>
2019-04-21 02:58:46 -0700

    versi 1.0001

---
+++ app/src/main/java/com/trianchatapps/Main/MyProfil.java
@@ -0,0 +1,15 @@
+package com.trianchatapps.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.trianchatapps.R;
+
+public class MyProfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profil);
+    }
+}
